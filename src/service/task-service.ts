d97f5bd125a35c9cf8a4ae18e10573bb59cbde59
liaolangsheng <liaolangsheng@mozat.com>
2021-02-18 16:24:55 +0800

    add init conifg

--- src/service/task-service.ts
+++ src/service/task-service.ts
@@ -6,12 +6,30 @@ import vscodeService, {VscodeService} from '../service/vscode-service';
 export class TaskService{
 
     timer: NodeJS.Timeout | undefined
-    config: Config | undefined;
+    config: Config = new Config();
     interval: number = 1;
     switch: boolean = true;
+    taskCode: number | undefined;
     
     setConfig(config : Config){
         this.config = config;
+    }
+
+    async loadConfig(){
+        try {
+            const workspace = vscodeService.getCurrentWorkspacePath();
+            const config : Config = await vscodeService.loadConfig(workspace);
+            this.interval = config.updateInterval / 1000;
+            if(!this.interval || this.interval <= 0){
+                this.showStatusBarMessage("no updateInterval")
+                return;
+            }
+            taskService.setConfig(config);
+            return config;
+        } catch (error) {
+            this.showStatusBarMessage("no config")
+            return undefined;
+        }
     }
 
     async restart(){
@@ -19,22 +37,8 @@ export class TaskService{
         this.startTimer();
     }
 
-    async startup(){
-        try {
-            const workspace = vscodeService.getCurrentWorkspacePath();
-            const config : Config = await vscodeService.loadConfig(workspace);
-            if(config && config.enable){
-                this.startTimer();
-            }
-        } catch (error) {
-            console.log("startup error:" + error);
-            vscodeService.setStatusBarMessage("git-robot:no config")
-            return;
-        }
-    }
-
     async startTimer(){
-        
+
         this.switch = true;
         if(this.timer){
             console.log("started");
@@ -42,21 +46,14 @@ export class TaskService{
         }
         console.log("starting");
 
-        try {
-            const workspace = vscodeService.getCurrentWorkspacePath();
-            const config : Config = await vscodeService.loadConfig(workspace);
-            this.interval = config.updateInterval / 1000;
-            if(!this.interval || this.interval <= 0){
-                vscodeService.setStatusBarMessage("git-robot:no updateInterval")
-                return;
-            }
-            taskService.setConfig(config);
-            await this.runTask();
-        } catch (error) {
-            console.log("start error:" + error);
-            vscodeService.setStatusBarMessage("git-robot:no config")
+        const config = await this.loadConfig();
+        if(!config){
             return;
         }
+        if(!this.validateConfig()){
+            return;
+        }
+        await this.runTask();
     }
 
     stopTimer(){
@@ -65,12 +62,56 @@ export class TaskService{
         }
         this.switch = false;
         this.timer = undefined;
-        console.log("stop");
-        vscodeService.setStatusBarMessage("git-robot:stop")
+        this.showStatusBarMessage("stop")
     }
 
     async runTask(){
+
+        let taskCodeTmp = this.generateTaskCode();
+
+        this.showStatusBarMessage(this.interval);
         
+        if(!this.reloadWorkspace()){
+            return;
+        }
+
+        if(this.interval <= 0) {
+            try {
+                this.interval = this.config.updateInterval / 1000;
+                this.showStatusBarMessage("syncing")
+                await gitService.sync();
+            } catch (error) {
+                vscodeService.showErrorMessage("git-robot:sync error" + error)
+                this.stopTimer();
+            }
+        } else {
+            this.interval--;
+        }
+
+        if(!this.switch){
+            this.showStatusBarMessage("stop")
+            return;
+        }
+
+        if(!this.validateTaskCode(taskCodeTmp)){
+            console.log("taskCode error, timer stop");
+            return;
+        }
+        this.timer = setTimeout(async () => {
+            await this.runTask();
+          }, 1000);
+    }
+
+    generateTaskCode(){
+        this.taskCode = new Date().getTime();
+        return this.taskCode;
+    }
+
+    validateTaskCode(taskCodeTmp: number | undefined){
+        return taskCodeTmp == this.taskCode;
+    }
+
+    validateConfig(){
         if(!this.config){
             console.log("no config, do not sync");
             vscodeService.setStatusBarMessage("git-robot:no config")
@@ -88,42 +129,21 @@ export class TaskService{
             vscodeService.setStatusBarMessage("git-robot:no updateInterval")
             return;
         }
+        return true;
+    }
 
+    showStatusBarMessage(msg : any){
+        vscodeService.setStatusBarMessage("git-robot:" + msg)
+    }
+
+    reloadWorkspace(){
         const workspace = vscodeService.getCurrentWorkspacePath();
         if(!workspace){
-            console.log("workspace is empty, can not sync");
-            vscodeService.setStatusBarMessage("git-robot:no workspace")
+            this.showStatusBarMessage("no workspace")
             return;
         }
-
-        console.log("interval = " + this.interval);
-        vscodeService.setStatusBarMessage("git-robot:" + this.interval)
-
-        if(this.interval <= 0) {
-            try {
-                this.interval = this.config.updateInterval / 1000;
-                vscodeService.setStatusBarMessage("git-robot:syncing")
-                gitService.setRepoDir(workspace);
-                await gitService.sync();
-            } catch (error) {
-                vscodeService.showErrorMessage("git-robot:sync error" + error)
-                this.stopTimer();
-            }
-        } else {
-            this.interval--;
-        }
-
-        // const newdate = new Date();
-        // let dateFormat = format(newdate, 'yyyy-MM-dd HH:mm:ss');
-        // console.log(dateFormat);
-        if(!this.switch){
-            console.log("switch off, do not sync");
-            vscodeService.setStatusBarMessage("git-robot:stop")
-            return;
-        }
-        this.timer = setTimeout(async () => {
-            await this.runTask();
-          }, 1000);
+        gitService.setRepoDir(workspace);
+        return workspace;
     }
 }
 
--- src/service/vscode-service.ts
+++ src/service/vscode-service.ts
@@ -15,9 +15,23 @@ export class VscodeService{
             return new Config();
         }
         const uri = vscode.Uri.file(workspace + "\\.git-robot\\config.json");
-        const configArray : Uint8Array =await vscode.workspace.fs.readFile(uri);
+        const configArray : Uint8Array = await vscode.workspace.fs.readFile(uri);
         const config : Config = JSON.parse(configArray.toString());
         return config;
+    }
+
+    async writeConfig(config: Config, workspace: string | undefined){
+        const uri = vscode.Uri.file(workspace + "\\.git-robot\\config.json");
+        const configUint8Array = this.stringToUint8Array(JSON.stringify(config));
+        await vscode.workspace.fs.writeFile(uri, configUint8Array);
+        return config;
+    }
+
+    async initConfig(){
+        const config : Config = new Config();
+        config.updateInterval = 5000;
+        config.enable = true;
+        await this.writeConfig(config, this.getCurrentWorkspacePath())
     }
 
     setStatusBarMessage(msg: string){
@@ -28,6 +42,16 @@ export class VscodeService{
         vscode.window.showErrorMessage(msg);
     }
 
+    
+    stringToUint8Array(str: string){
+        let arr = [];
+        for (let i = 0, j = str.length; i < j; ++i) {
+            arr.push(str.charCodeAt(i));
+        }
+    
+        let tmpUint8Array = new Uint8Array(arr);
+        return tmpUint8Array
+    }
 }
 
 const vscodeService = new VscodeService();
